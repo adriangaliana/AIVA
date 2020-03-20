78af517505c823291af2503a97bc4533a3fc2193
Pedro Miguel Martínez <60963546+pedro-miguel-martinez@users.noreply.github.com>
2020-03-20 22:48:08 +0100

    Add files via upload

---
+++ recognizer.py
@@ -0,0 +1,29 @@
+
+# Imports necesarios
+import numpy as np
+import cv2
+import tensorflow as tf
+
+class recognize():
+    def __init__(self):
+        return
+
+    def validate(self):
+        return
+
+    def recognize(self):
+
+        """
+        El método reconocedor de imagen devuelve un entero, 1 si la imagen sí se puede reconocer o un 0 si la imagen no
+        se puede reconocer.
+        """
+        return
+
+    def _preprocess(self):
+        return
+
+    def _count(self):
+        return
+
+    def _position(self):
+        return
