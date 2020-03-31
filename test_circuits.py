11297b1b02ebb70260da4fb72f73a90a0c37e2d3
Pedro Miguel Martínez <60963546+pedro-miguel-martinez@users.noreply.github.com>
2020-03-31 16:37:14 +0200

    Update test_circuits.py

--- test_circuits.py
+++ test_circuits.py
@@ -3,19 +3,16 @@ import numpy as np
 from reconocedor_mockup import reconocer_imagen, contar_condensadores, posicionar_condensadores
 
 class Test_mockup (unittest.TestCase):
+    
     def test_reconocer (self):
         # Creamos una imagen de prueba
         # matriz = [[1,2,3],[4,5,6],[7,8,9]]
         # im = np.array(matriz, dtype=np.uint8)
-        
         """
-        
         Este primer test es el que comprueba si pasándole cualquier tipo de imagen se devuelve 
         un TRUE cuando hay condensadores en la imagen de un circuito integrado y FALSE en caso
         contrario.
-        
-        """
-           
+        """  
         im = './Train/rec1-1.jpg' # imagen ejemplo
         res = reconocer_imagen(im)
         print(res)
@@ -25,44 +22,26 @@ class Test_mockup (unittest.TestCase):
         # Creamos una imagen de prueba
         # matriz = [[1,2,3],[4,5,6],[7,8,9]]
         # im = np.array(matriz, dtype=np.uint8)
-                
         """
-        
         Este segundo test es el que comprueba que al pasarle una imagen se devuelve 
         un TRUE cuando hay condensadores y el número que devuelve la función del módulo
         principal es un entero; y FALSE en caso contrario.
-        
         """
-           
         im = './Train/rec1-1.jpg'
         res = contar_condensadores(im)
         print(type(res))
         self.assertTrue((type(res) is int) == True)
 
-
-    def test_contar (self):
+    def test_posicionar (self):
         # Creamos una imagen de prueba
         # matriz = [[1,2,3],[4,5,6],[7,8,9]]
         # im = np.array(matriz, dtype=np.uint8)
-        
         """
-        
         Finalmente, el tercer test es el que comprueba que al pasarle una imagen se devuelve 
         un TRUE cuando hay condensadores y se obtiene un número entero en las dos posiciones del res
         que devuelve la función del módulo principal, las cuales marcan la posición del centro radial
         del condensador encontrado; y FALSE en caso contrario.
-        
         """
-        
-        im = './Train/rec1-1.jpg'
-        res = contar_condensadores(im)
-        print(type(res))
-        self.assertTrue((type(res) is int) == True)
-
-    def test_contar (self):
-        # Creamos una imagen de prueba
-        # matriz = [[1,2,3],[4,5,6],[7,8,9]]
-        # im = np.array(matriz, dtype=np.uint8)
         im = './Train/rec1-1.jpg'
         res = posicionar_condensadores(im)
         print(res)
